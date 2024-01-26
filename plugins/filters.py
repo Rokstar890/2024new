9bad2fbdd32b2a01885b51c46d861d356f9aa647
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-01-26 23:40:09 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -189,7 +189,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot")]
+                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/rokstar890/2024new")]
             ])
 
             await query.message.edit_text(
